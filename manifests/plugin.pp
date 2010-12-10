31afacb9100ed4bc3bbaea21624e7454e25095ce
Tim Sharpe <tim@github.com>
2010-12-10 14:05:11 +1100

    Whoops

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,6 +1,5 @@
 define rabbitmq::plugin($ensure='present', $plugin_dir='', $version) {
 	$plugin_url = "http://www.rabbitmq.com/releases/plugins/v${version}/${name}-${version}.ez"
-	$plugin_path = "${plugin_dir}/${name}-${version}.ez"
 
 	if $plugin_dir == '' {
 		$_plugin_dir = "/usr/lib/rabbitmq/lib/rabbitmq_server-${version}/plugins"
@@ -8,9 +7,11 @@ define rabbitmq::plugin($ensure='present', $plugin_dir='', $version) {
 		$_plugin_dir = $plugin_dir
 	}
 
+	$plugin_path = "${_plugin_dir}/${name}-${version}.ez"
+
 	if $ensure == "present" {
 		exec { "Downloading RabbitMQ plugin ${name}":
-			command => "/usr/bin/wget ${plugin_url} -O ${plugin_dir}",
+			command => "/usr/bin/wget ${plugin_url} -O ${_plugin_dir}",
 			creates => $plugin_path,
 		}
 	} else {
